01d4e111443d08f49c4fd0c380ef588b1caddf90
Taiwo  Gbemisola Bello <tbello@ecobank.com>
2021-10-10 09:07:49 +0100

    Feat: Designing single web angular application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './WelcomePage/about/about.component';
+import { ClientsComponent } from './WelcomePage/clients/clients.component';
+// import { EditmyprofileComponent } from './Customer/editmyprofile/editmyprofile.component';
+// import { OnboardingComponent } from './Customer/onboarding/onboarding.component';
+import { SigninComponent } from './WelcomePage/signin/signin.component';
+import { SignupComponent } from './WelcomePage/signup/signup.component';
+// import { WalletComponent } from './Customer/dash-board/wallet/wallet.component';
+import { OurSolutionComponent } from './WelcomePage/our-solution/our-solution.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+     { path: 'signin', component: SigninComponent },
+     { path: 'signup', component: SignupComponent },
+     { path: 'about', component: AboutComponent },
+     { path: 'clients', component: ClientsComponent },
+     { path: 'oursolution', component: OurSolutionComponent },
+    //  { path: 'dashboard', component: },
+
+    //  { path: 'onboarding', component: OnboardingComponent },
+    //  { path: 'editprofile', component: EditmyprofileComponent },
+    //  { path: 'wallet', component: WalletComponent }
+   
+     // TODO firstweb page consist of signup, signin, about and client. After the customer is logged in then the customer can have access
+     // to onboarding, editprofile, wallet, orders, signout
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './WelcomePage/about/about.component';
+import { SigninComponent } from './WelcomePage/signin/signin.component';
+import { SignupComponent } from './WelcomePage/signup/signup.component';
+import { OurSolutionComponent } from './WelcomePage/our-solution/our-solution.component';
+import { OnboardingComponent } from './Customer/onboarding/onboarding.component';
+// import { EditmyprofileComponent } from './Customer/editmyprofile/editmyprofile.component';
+import { ClientsComponent } from './WelcomePage/clients/clients.component';
+import { CartComponent } from './Customer/dash-board/cart/cart.component';
+import { WalletComponent } from './Customer/dash-board/wallet/wallet.component';
+// import { MyordersComponent } from './Customer/myorders/myorders.component';
+import { SignoutComponent } from './Customer/signout/signout.component';
+import { DashBoardComponent } from './Customer/dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SigninComponent,
+    SignupComponent,
+    OurSolutionComponent,
+    OnboardingComponent,
+    // EditmyprofileComponent,
+    ClientsComponent,
+    CartComponent,
+    WalletComponent,
+    // MyordersComponent,
+    SignoutComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
